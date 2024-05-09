e7ed8d5c4952e4d7c2ebcd3ec4b3cf8d4087d69c
Victor Hugo <vhclaudiogomes@gmail.com>
2024-05-09 17:31:20 -0300

    to csv

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+
 import yaml
 from models import TeamModel as Time
 from models import PlayerModel as Jogador
@@ -41,8 +43,27 @@ def get_base_info(team_data):
     return jogadores_base_info
 
 
-if __name__ == '__main__':
-    team_file_path = 'teamsyamls/academia_ven.yml'
+import os
+import csv
 
-    team_infos = get_team_info(team_file_path)
-    jogadores_base = get_base_info(team_infos)
+import os
+import csv
+
+if __name__ == '__main__':
+    folder_path = 'yamls/'
+    csv_file_path = 'jogadores_base.csv'
+
+    with open(csv_file_path, 'w', newline='', encoding='utf-8') as csv_file:
+        csv_writer = csv.writer(csv_file)
+        csv_writer.writerow(['Nome', 'Estrela', 'Pais', 'Time', 'Idade', 'Posicao', 'Titular', 'Top Mundial', 'Lado', 'Caracteristica 1', 'Caracteristica 2', 'Extra'])
+
+        for filename in os.listdir(folder_path):
+            if filename.endswith('.yml'):
+                team_file_path = os.path.join(folder_path, filename)
+                team_data = read_yaml(team_file_path)
+                jogadores_base = get_base_info(team_data)
+                for jogador in jogadores_base:
+                    caracteristica_1 = jogador.caracteristicas[0] if jogador.caracteristicas else None
+                    caracteristica_2 = jogador.caracteristicas[1] if len(jogador.caracteristicas) > 1 else None
+                    csv_writer.writerow([jogador.nome, jogador.estrela, jogador.pais, jogador.time, jogador.idade, jogador.posicao, jogador.titular, jogador.top_mundial, jogador.lado, caracteristica_1, caracteristica_2, jogador.extra])
+
